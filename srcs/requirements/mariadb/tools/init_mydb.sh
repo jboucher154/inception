1d29ca5dc603ee91729f5069ac5d498e9387adc2
Jenny Boucher <jboucher154@gmail.com>
2024-02-05 17:30:26 +0200

    add fixed scripts and docker-compose for mariadb

--- srcs/requirements/mariadb/tools/init_mydb.sh
+++ srcs/requirements/mariadb/tools/init_mydb.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export DB_ROOT_PASSWORD=test
+export DB_USER=test_user
+export DB_USER_PASSWORD=test1
+
 # make sure environment vars are set
 if [ -z "$DB_ROOT_PASSWORD" ]; then
   echo "Error: DB_ROOT_PASSWORD environment variable is not set."
@@ -11,45 +15,28 @@ if [ -z "$DB_USER" ] || [ -z "$DB_ROOT_PASSWORD" ]; then
   exit 1
 fi
 
-# might want to change ownership of database files in /run/mysqld /var/lib/mysqld
-mkdir -p /var/log/mysqld
-chown -R mysql:mysql /var/log/mysqld
+# to change ownership of database files in /run/mysqld /var/lib/mysqld
+mkdir -p /var/lib/mysql /run/mysqld
+chown -R mysql:mysql /var/lib/mysql
 chown -R mysql:mysql /run/mysqld
 
-#start maraidb in background
-echo "Starting database in background"
-mysqld &
-#give time for it to load
-sleep 5
 #give commands to database using root user, no password to begin with
-echo "Checking status of 'my_inception_db'"
-if mysql -u root -p"$DB_ROOT_PASSWORD" -e "USE my_inception_db;" 2>/dev/null; then
-  echo "Database 'my_inception_db' already intialized."
-else
-	echo "Intializing database and users"
-	mysql -u root -p << EOF
-	CREATE DATABASE IF NOT EXISTS my_inception_db;
-	USE my_inception_db;
+echo "Intializing database"
+mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql --rpm >/dev/null
 
-	ALTER USER 'root' IDENTIFIED BY '$DB_ROOT_PASSWORD' HOST 'localhost';
-	GRANT ALL PRIVILEGES ON my_inception_db.* TO 'root'@'%';
-	CREATE USER '$DB_USER' IDENTIFIED BY '$DB_USER_PASSWORD';
-	GRANT ALL PRIVILEGES ON my_inception_db.* TO '$DB_USER'@'%';
+echo "Intializing database and users"
+mysqld --user=mysql --bootstrap << EOF
+USE mysql;
+FLUSH PRIVILEGES;
 
-	FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
+CREATE DATABASE IF NOT EXISTS my_inception_db CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '${DB_USER}'@'%' IDENTIFIED by '${DB_USER_PASSWORD}';
+GRANT ALL PRIVILEGES ON my_inception_db.* TO '${DB_USER}'@'%';
+
+FLUSH PRIVILEGES;
 EOF
-fi
 
-# shutdown the database
-echo "Shutting down database"
-mysqladmin -u root -p"$DB_ROOT_PASSWORD" shutdown
 
-#runs server in safemode, automatic restart if issues encountered
-# echo "Starting database in safe mode"
-# mysqld_safe --defaults-file=/etc/my.cnf
-
-## root user not log in???
-# while [ true ] 
-# do
-# 	sleep 1
-# done
+# GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'%';
+# GRANT SELECT ON mysql.* TO '${DB_USER}'@'%';
